e493f7b62390eb7c27135636382f5ae1dd101079
theanh <anhdtph18035@fpt.edu.vn>
2022-11-15 08:54:40 +0700

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,90 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs')
+const url = require('url')
+
+var db = 'mongodb+srv://admin:ntcz3NtuBmDGzQQq@cluster0.bbvmc.mongodb.net/asmnet?retryWrites=true&w=majority'
+const mongoose = require('mongoose');
+const e = require("express");
+mongoose.connect(db).catch(error =>{
+  console.log("Có lỗi xảy ra")
+});
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+const truyenSchema = new mongoose.Schema({
+  name: 'string',
+  chart: 'string',
+  avatar: 'string',
+  Category: 'string'
+
+});
+const commentSchema = new mongoose.Schema({
+  noidung: 'string',
+  time: 'string'
+});
+const userSchema = new mongoose.Schema({
+  username: 'string',
+  password: 'string',
+  email: 'string',
+  fullname: 'string'
+
+});
+const Truyen = mongoose.model('truyens',truyenSchema);
+router.post('/uploads', function (req,res) {
+  var name = req.body.name;
+  var chart = req.body.chart;
+  var avatar = req.body.avatar;
+  var Category = req.body.Category;
+  console.log(name);
+  console.log(chart);
+  console.log(avatar);
+  console.log(Category);
+
+  const data = new Truyen({
+    name: name,
+    chart: chart,
+    avatar: avatar,
+    Category:Category
+  });
+
+  data.save(function (err) {
+    if (err) return handleError(err);
+    res.render('index', {
+      title: 'Thêm',
+      message: 'Đã thêm'
+    })
+    console.log('Da them')
+  });
+});
+router.get('/gettruyen',function (req,res) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET');
+  Truyen.find({}, function (err,data){
+    res.send(data);
+  })
+
+});
+router.get('/getComments', function (req, res) {
+
+  const CommentList = mongoose.model('comments', commentSchema);
+
+  CommentList.find({}, function (error, result) {
+    res.send(result);
+  })
+});
+router.get('/getuser', function (req, res) {
+  const userList = mongoose.model('users', userSchema);
+  userList.find({}, function (error, result) {
+    res.send(result);
+  })
+});
+router.post('/postuser',function (req,res) {
+  const userList = mongoose.model('users', userSchema);
+  userList.find({}, function (error, result) {
+    res.send(result);
+  })
+  });
+module.exports = router;
